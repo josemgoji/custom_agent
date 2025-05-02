0632443066a302a084855cf4bf31a2a26fcf3f6a
Jose Manuel <jgomezjimenez042@gmail.com>
2025-05-01 23:03:29 -0500

    agregar import load_dotenv

--- src/create_VDB.py
+++ src/create_VDB.py
@@ -1,9 +1,12 @@
 import os
+from dotenv import load_dotenv
 from langchain_chroma import Chroma 
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 from pathlib import Path
+
+load_dotenv()
 
 data_folder = Path('./data')
 docs = []
